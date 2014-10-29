b60dc023360eee09a547f8169f15d6ac915a70f5
John Barry <jackvbarry@gmail.com>
2014-10-29 10:15:58 -0400

    Buffered Reader
    Developed Main class and method to make use of the Buffered Reader and
    file InputStreams

--- Main.java
+++ Main.java
@@ -6,48 +6,79 @@ Software development 1 Project 3
 
 package com.company;
 
-import java.io.BufferedReader;
+import java.io.*;
 
 public class Main {
 
-    public static void InitializeAndWelcome(){
-        /* Print welcome message */
-        System.out.println("Welcome to the Annual Lawn Game Olympics, Please read the following instructions." +
-                "\nEnter the letter e or the word events for a list of today's events.\nEnter the letter o or the word olympians for a list of the competing olympians." +
-                "\nEnter the letter h or the word help to display some helpful tips if you are having trouble. ");
-
-    }
     public static void main(String[] args) {
         /*call initialize and welcome method*/
 
-        boolean Quit = false;
-        while (Quit = false) {
+       try{
+           BufferedReader BuffRead= new BufferedReader(new FileReader(args[0]));
+           String line;
+           boolean firstline = false;
+           while((line=BuffRead.readLine())!=null)
 
+           {
+               if (line.equals("lgoo") || line.equals("LGOO")) {
+                   System.out.println("File Accepted");
+                   break;
+               } else {
+                   System.out.println("Error, File is incorrect");
+                   break;
+               }
+           }
+            }catch(IOException ioe){
+                System.out.println("IO Exception");
+           }
+
+
+        BufferedReader BuffRead = new BufferedReader(new FileReader(File));
             Main.InitializeAndWelcome();
-        /*Create userInput variable based on the first position in the args array*/
-            String userInput = args[0];
 
-
-            if (userInput.equals("e") || userInput.equals("events")) {
+        /*Create userInput variable*/
+        String userMove;
+            BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+               try{
+                   userMove= userInput.readLine();
+               }catch(IOException ioe){
+                   System.out.println("Error");
+                   return;
+               }
+            while (userMove != "quit" || userMove != "q")
+            if (userMove.equals("e") || userMove.equals("events")) {
             /*make a call to displayEvents method*/
                 EventManager();
-            } else if (userInput.equals("o") || userInput.equals("olympians")) {
+            } else if (userMove.equals("o") || userMove.equals("olympians")) {
             /*make a call to the displayOlympians method*/
                 OlympianManager();
-            } else if (userInput.equals("h") || userInput.equals("help")) {
+            } else if (userMove.equals("h") || userMove.equals("help")) {
                 displayHelp();
             }
-            else if (userInput.equals("t") || userInput.equals("teams")){
-
+            else if (userMove.equals("t") || userMove.equals("teams")){
+                getTeams();
             }
-            else if (userInput.equals("q") || userInput.equals("quit")) {
-                    Quit = true;
+            else if (userMove.equals("q") || userMove.equals("quit")) {
                     System.out.println("Thank you for playing, goodbye");
+                    EndProgram();
             } else {
-                System.out.println("Invalid input, entering the letters e, o, or h or the words events, olympians, or help as they appear before you.");
-            }
+                System.out.println("Invalid input, entering the letters e, o, h, or q or the words events, olympians, help, or quit as they appear before you.");
+                try{
+                    userMove = userInput.readLine();
+            }catch(IOException ioe){
+                    System.out.println("Error");
+                    return;
+                }
         }
     }
+
+    public static void InitializeAndWelcome(){
+        /* Print welcome message */
+               System.out.println("Welcome to the Annual Lawn Game Olympics, Please read the following instructions." +
+                       "\nEnter the letter e or the word events for a list of today's events.\nEnter the letter o or the word olympians for a list of the competing olympians." +
+                       "\nEnter the letter h or the word help to display some helpful tips if you are having trouble. ");
+
+           }
 
     public static void EventManager(){
         EventManager Manage = new EventManager();
@@ -60,6 +91,10 @@ public class Main {
         OlympManage.displayOlympians();
     }
 
+   public static void teamManager(){
+       TeamManager teams = new TeamManager();
+       teams.displayTeams();
+   }
     public static void displayHelp(){
         System.out.println("Your input is case sensitive, make sure you are using lowercase letters and try again.\n" +
                 "Remember, entering the letter e or the word events will display a list of today's events.\n" +
@@ -67,4 +102,9 @@ public class Main {
                 "Entering the letter h or the word help brings you to this message.\n" +
                 "Entering anything other than the above arguments will not work.");
     }
+    public static void EndProgram(){
+        System.exit(1);
+    }
+
 }
+
